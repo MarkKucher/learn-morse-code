04637be01afcbf093a166873bef2ed532f61b067
MarkKucher <markkucher100@gmail.com>
2024-02-20 20:16:28 +0200

    wrong lang

--- client/next.config.js
+++ client/next.config.js
@@ -3,8 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   i18n: {
-    locales: ["en", "ua"],
-    defaultLocale: "ua",
+    locales: ["en", "uk"],
+    defaultLocale: "uk",
   }
 }
 
